eb5588b21baa411f9afe3adfe2794c1ec94ebb6a
mathues henrique <matheus.honascimento@senacsp.edu.br>
2023-06-20 17:21:57 -0300

    CalculoMedia

---
+++ appcalculodemedia/appcalculodemedia/FRMcaluloMedia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appcalculodemedia
+{
+    public partial class TxtNota1 : Form
+    {
+        public TxtNota1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnCalular_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
