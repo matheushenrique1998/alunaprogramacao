24ac6f6cf758bf101e9d836f9b85069525083c77
mathues henrique <matheus.honascimento@senacsp.edu.br>
2023-06-21 17:25:39 -0300

    cpf

---
+++ cpf/cpf/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cpf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int cpf[+] = { 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7 }
+
+       in mult1[+] = { 10, 9, 8, 7, 6, 5, 4, 3, 2 }
+
+       int mult2[+] = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 }
+
+       int = 0
+
+       int resto
+
+       
+
+       if(cpf[0] == cpf[1] e cpf[1] == cpf[2] e cpf[2] == cpf[3] e cpf[3] == cpf[4] e  cpf[4] == cpf[5] e cpf[5] == cpf[6] e cpf[6] == cpf[7] e cpf[7] == cpf[8] e
+
+       cpf[8] == cpf[9] e cpf[9] == cpf[10]){
+                    Console.WriteLine("cpf invalido")
+
+       }
+                else{
+                    para(i = 0 ; i < 9; i++)
+	   	soma += (cpf[+] * mult1[+])
+
+       }
+        resto = (soma*10)%11 
+	   if(resto != cpf[9])
+        {
+                    Console.WriteLine("cpf invalido")
+
+       }
+        else{
+	   	soma=10
+	   	if(i= 0 ; i<10 ;i++)
+	   	soma =(cpf[+]* mult2[+])
+
+       }
+    resto = (soma*10)%11
+	   if(resto != cpf[10])
+    {
+        resto = (210 * 10) / 11
+
+           if(resto == 10){
+            resto = 0
+
+      Console.WriteLine("cpf invalido")
+
+       }
+        {
+               Console.WriteLine("cpf  valido")
+
+       }
+    }
+}   
+}
+        }
+    }
+}
