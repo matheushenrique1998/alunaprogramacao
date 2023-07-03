5c907b2bde9edf3098b6a0e55c129c97dcc5ab75
mathues henrique <matheus.honascimento@senacsp.edu.br>
2023-07-03 15:21:52 -0300

    r

--- ValidacaoCpfCnpj/ValidacaoCpfCnpj/ValidacaoCpfCnpj.Designer.cs
+++ ValidacaoCpfCnpj/ValidacaoCpfCnpj/ValidacaoCpfCnpj.Designer.cs
@@ -28,30 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtvalidacao = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.rbcpf = new System.Windows.Forms.RadioButton();
+            this.rbcnpj = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
-            // label1
+            // txtvalidacao
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 96);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "entre";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(130, 113);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(175, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtvalidacao.Location = new System.Drawing.Point(130, 113);
+            this.txtvalidacao.Name = "txtvalidacao";
+            this.txtvalidacao.Size = new System.Drawing.Size(175, 20);
+            this.txtvalidacao.TabIndex = 3;
+            this.txtvalidacao.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtvalidacao.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numero_click);
             // 
             // button1
             // 
@@ -73,61 +64,58 @@
             this.button2.TabIndex = 5;
             this.button2.Text = "c";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // radioButton1
+            // rbcpf
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(121, 22);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(65, 28);
-            this.radioButton1.TabIndex = 6;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "CPF";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged_2);
+            this.rbcpf.AutoSize = true;
+            this.rbcpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbcpf.Location = new System.Drawing.Point(121, 22);
+            this.rbcpf.Name = "rbcpf";
+            this.rbcpf.Size = new System.Drawing.Size(65, 28);
+            this.rbcpf.TabIndex = 6;
+            this.rbcpf.TabStop = true;
+            this.rbcpf.Text = "CPF";
+            this.rbcpf.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // rbcnpj
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.Location = new System.Drawing.Point(265, 22);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(64, 28);
-            this.radioButton2.TabIndex = 7;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "cnpj";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            this.rbcnpj.AutoSize = true;
+            this.rbcnpj.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbcnpj.Location = new System.Drawing.Point(265, 22);
+            this.rbcnpj.Name = "rbcnpj";
+            this.rbcnpj.Size = new System.Drawing.Size(64, 28);
+            this.rbcnpj.TabIndex = 7;
+            this.rbcnpj.TabStop = true;
+            this.rbcnpj.Text = "cnpj";
+            this.rbcnpj.UseVisualStyleBackColor = true;
             // 
             // ValidacaoCpfCnpj
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(428, 284);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.rbcnpj);
+            this.Controls.Add(this.rbcpf);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtvalidacao);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ValidacaoCpfCnpj";
             this.Text = "nome de app";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.ValidacaoCpfCnpj_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtvalidacao;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton rbcpf;
+        private System.Windows.Forms.RadioButton rbcnpj;
     }
 }
 
--- ValidacaoCpfCnpj/ValidacaoCpfCnpj/ValidacaoCpfCnpj.cs
+++ ValidacaoCpfCnpj/ValidacaoCpfCnpj/ValidacaoCpfCnpj.cs
@@ -5,9 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ValidacaoCpfCnpj
@@ -21,177 +23,169 @@ namespace ValidacaoCpfCnpj
 
         private void label1_Click(object sender, EventArgs e)
         {
-            if (radioButton1.Checked)
+  
+        }
+        private void button1_Click(object sender, EventArgs e) //Quando apertar OK
+        {
+            if (txtvalidacao.Text == "" && !rbcpf.Checked && !rbcnpj.Checked)
             {
-                string numerostring = TextBox1.txt;
-                int[]= new int[11];
-
-            }
-            if (letras(numeroSting))
-            {
-                MessageBox.Show("o cpf aceito apenas numero", "Aviso", MessageBox.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Campo em branco", "bug", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 return;
             }
-            if (numeroSting.length >= 15)
+            else if (txtvalidacao.Text == "" && rbcpf.Checked)
             {
-                MessageBox.Show("o  número de caract foi utrapassdo", "Error", MessageBox.OK, MessageBoxIcon.Error);
+                MessageBox.Show("coloque o número do seu cpf", "error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 return;
-
-
+            }
+            else if (txtvalidacao.Text == "" && rbcnpj.Checked)
+            {
+                MessageBox.Show("coloque o número do seu cnpj","error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
             }
 
-            private void button1_Click(object sender, EventArgs e)
+            if (rbcpf.Checked)
             {
-                if (textBox1.Text == "")
+                if(txtvalidacao.Text.Length < 11)
                 {
-                    MessageBox.Show("Campo em branco", "bug", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    MessageBox.Show("coloque todos os digitos do seu cpf", "error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                     return;
-
                 }
+                string numerostring = txtvalidacao.Text;
+                int[] cpf = numerostring.Select(numero => Convert.ToInt32(numero.ToString())).ToArray();
 
+                int[] multiplicadoresDigito1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+                int[] multiplicadoresDigito2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
 
-
-            }
-
-            private void Form1_Load(object sender, EventArgs e)
-            {
-
-
-                if (radioButton1.Checked)
-                { radioButton1.Checked = true; }
-
-                bool ValidarCPF(string cpf)
+                int soma = 0;
+             
+                for (int i = 0; i < multiplicadoresDigito1.Length; i++)
                 {
-                    cpf = cpf.Replace(".", "").Replace("-", "");
-
-
-
-                    if (cpf.Length != 11)
-                        return false;
-
-
-
-                    // Verifica se todos os dígitos são iguais
-                    bool todosDigitosIguais = true;
-                    for (int i = 1; i < cpf.Length; i++)
-                    {
-                        if (cpf[i] != cpf[0])
-                        {
-                            todosDigitosIguais = false;
-                            break;
-                        }
-                    }
-                    if (todosDigitosIguais)
-                        return false;
-
-
-
-                    int[] multiplicadoresDigito1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-                    int[] multiplicadoresDigito2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-
-
-
-                    int soma = 0;
-                    for (int i = 0; i < multiplicadoresDigito1.Length; i++)
-                    {
-                        soma += multiplicadoresDigito1[i] * int.Parse(cpf[i].ToString());
-                    }
-
-
-
-                    int resto = soma % 11;
-                    int digito1 = (resto < 2) ? 0 : 11 - resto;
-
-
-
-                    if (int.Parse(cpf[9].ToString()) != digito1)
-                        return false;
-
-
-
-                    soma = 0;
-                    for (int i = 0; i < multiplicadoresDigito2.Length; i++)
-                    {
-                        soma += multiplicadoresDigito2[i] * int.Parse(cpf[i].ToString());
-                    }
-
-
-
-                    resto = soma % 11;
-                    int digito2 = (resto < 2) ? 0 : 11 - resto;
-
-
-
-                    return int.Parse(cpf[10].ToString()) == digito2;
+                    soma += cpf[i] * multiplicadoresDigito1[i];
                 }
+                int resto = soma % 11;
+                int digito1 = (resto < 2) ? 0 : 11 - resto;
 
-
-
+                int soma2 = 0;
+                for (int i = 0; i < multiplicadoresDigito2.Length; i++)
                 {
-                    bool validaCpf = false;
-
-
-
-                    while (!validaCpf)
-                    {
-                        ("Digite o CPF: ");
-                        string cpf = Console.ReadLine();
-
-
-
-                        if (cpf.Length == 11)
-                        {
-                            bool contemLetras = false;
-                            foreach (char c in cpf)
-                            {
-                                if (!char.IsDigit(c))
-                                {
-                                    contemLetras = true;
-                                    break;
-                                }
-                            }
-
-
-
-                            if (!contemLetras && ValidarCPF(cpf))
-                            {
-                                validaCpf = true;
-                                cpf = cpf.Insert(3, ".").Insert(7, ".").Insert(11, "-");
-                                Console.WriteLine("CPF Válido: " + cpf);
-                            }
-                            else
-                            {
-                                ("CPF Inválido. Digite apenas números.");
-                            }
-                        }
-                        else
-                        {
-                            ("CPF Inválido. O CPF deve conter 11 dígitos.");
-                        } if (radioButton2.Checked)
-                        { radioButton2.Checked = false; }
-
-
-
-                    }
-
-
+                    soma2 +=cpf[i] * multiplicadoresDigito2[i];
                 }
+                int resto2 = soma2 % 11;
+                int digito2 = (resto2 < 2) ? 0 : 11 - resto2;
 
+                string cpfFormatado = $"{numerostring.Substring(0, 3)}.{numerostring.Substring(3, 3)}.{numerostring.Substring(6, 3)}-{numerostring.Substring(9, 2)}";
 
+                if (cpf[9] == digito1 && cpf[10] == digito2)
+                {
+                    MessageBox.Show($"o cpf: {cpfFormatado} é valido","valido", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show($"o cpf: {cpfFormatado} é invalido","invalido", MessageBoxButtons.OK);
+                }
             }
-            private void radioButton2_CheckedChanged(object sender, EventArgs e)
+            else if (rbcnpj.Checked)
             {
+                if (txtvalidacao.Text.Length < 14)
+                {
+                    MessageBox.Show("coloque todos os digitos do seu cnpj", "error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                string cnpjString = txtvalidacao.Text;
+                int[] cnpj = cnpjString.Select(numero => Convert.ToInt32(numero.ToString())).ToArray();
 
+                int[] multiplicadoresDigito1 = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+                int[] multiplicadoresDigito2 = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
 
+                int soma = 0;
+                for (int i = 0; i < multiplicadoresDigito1.Length; i++)
+                {
+                    soma += cnpj[i] * multiplicadoresDigito1[i];
+                }
+                int resto = soma % 11;
+                int digito1 = (resto < 2) ? 0 : 11 - resto;
 
-            }
+                int soma2 = 0;
+                for (int i = 0; i < multiplicadoresDigito2.Length; i++)
+                {
+                    soma2 += cnpj[i] * multiplicadoresDigito2[i];
+                }
+                int resto2 = soma2 % 11;
+                int digito2 = (resto2 < 2) ? 0 : 11 - resto2;
 
-            private void radioButton1_CheckedChanged_2(object sender, EventArgs e)
-            {
+                string cnpjFormatado = $"{cnpjString.Substring(0, 2)}.{cnpjString.Substring(2, 3)}.{cnpjString.Substring(5, 3)}/{cnpjString.Substring(8, 4)}-{cnpjString.Substring(12, 2)}";
 
+                if (cnpj[12] == digito1 && cnpj[13] == digito2)
+                {
+                    MessageBox.Show($"o cnpj: {cnpjFormatado} é valido", "valido", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show($"o cnpj: {cnpjFormatado} é invalido", "invalido", MessageBoxButtons.OK);
+                }
             }
         }
-    } }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                                       
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged_2(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ValidacaoCpfCnpj_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void numero_click(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // Impede a digitação de caracteres não numéricos
+            }
+            int maxLength = 0;
+
+            if (rbcpf.Checked) //se o botão de cpf está ativo
+            {
+                maxLength = 11;
+            }
+            else if (rbcnpj.Checked) //se o botão de cnpj está ativo
+            {
+                maxLength = 14;
+            }
+            else
+            {
+                return; // Nenhum RadioButton está marcado, não faz nada
+            }
+            if (txtvalidacao.Text.Length >= maxLength && e.KeyChar != '\b') // Verifica se atingiu o limite e a tecla pressionada não é "Backspace"
+            {
+                txtvalidacao.SelectionStart = txtvalidacao.Text.Length; // Mantém o cursor no final do texto
+                e.Handled = true; // Impede que o caractere seja digitado
+                MessageBox.Show("O limite de caracteres foi excedido!", "Erro!", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
     
         
     
