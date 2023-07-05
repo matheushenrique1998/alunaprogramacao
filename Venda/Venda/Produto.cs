d51b7fe8d60d5dc81c8aa2bd6bb30364d942b067
mathues henrique <matheus.honascimento@senacsp.edu.br>
2023-07-05 17:16:07 -0300

    i

---
+++ Venda/Venda/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venda
+{
+    internal class Produto
+    {
+        public Produto() { }
+        public Produto(Guid id) { }
+        public Guid Id { get; set; }
+        public string Pedido { get; set;
+        }
+    }
+}
