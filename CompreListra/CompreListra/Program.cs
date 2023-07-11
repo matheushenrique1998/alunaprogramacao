f6461ff42e3b3364f82ff2ea554a0225a1efa75b
matheushenrique1998 <134648358+matheushenrique1998@users.noreply.github.com>
2023-07-11 17:21:15 -0300

    i

---
+++ CompreListra/CompreListra/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alortmosdiversoscsharp
+{
+    internal class ListaDeCompras
+    {
+        static void Main()
+        {
+            List<string> listaDeCompra = new List<string>();
+            Console.Write(":::::::: Programa de lista de Compras :::::::::");
+            while (true)
+            {
+                Console.WriteLine("Digite um item para para adivionar á lista (ou 'parar' sair ): ");
+                string item = Console.ReadLine();
+                if (item.ToLower() == "parar")
+                {
+                    break;
+                }
+                listaDeCompra.Add(item);
+                Console.WriteLine("Item adicionado com sucesso!");
+            }
+
+            Console.Write("-----sua lista de Compras -----");
+            //imprimir a lista ,porem estaja o texto "lista está vazia",
+            //caso contrario i,primir todo os tens na tela
+
+            if (listaDeCompra.Count == 0)
+            {
+                Console.WriteLine("lista esta está vazia");
+            }
+            else
+            {
+                foreach (string lista in listaDeCompra)
+                {
+
+                    Console.WriteLine(lista);
+                }
+            }
+        }
+        
+    }
+}
---
+++ CompreListra/CompreListra/produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrintacaoObjeto
+{
+    internal class Livro
+    {
+        public string nome;
+        
+        public double Preco;
+        public Livro( string nome= "", double preco = 0.0)
+        {
+            nome=nome
+            preco = preco;
+
+        }
+
+
+        public void ImprimirDetalhes()
+        {
+            Console.WriteLine("----");
+            Console.WriteLine("nome : {0}", nome);
+           
+          Console.WriteLine("Preço : {0:C}", Preco);
+            Console.WriteLine("----");
+
+
+        }
+
+    }
+}
