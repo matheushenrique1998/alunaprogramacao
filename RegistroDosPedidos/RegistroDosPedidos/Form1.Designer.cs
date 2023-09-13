f0a49409bf42b3e3254d31c9d9c600b585b016c1
matheushenrique1998 <134648358+matheushenrique1998@users.noreply.github.com>
2023-09-13 15:11:22 -0300

    to

--- RegistroDosPedidos/RegistroDosPedidos/Form1.Designer.cs
+++ RegistroDosPedidos/RegistroDosPedidos/Form1.Designer.cs
@@ -32,24 +32,24 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.btnadicionar = new System.Windows.Forms.Button();
-            this.btnatuzlizar = new System.Windows.Forms.Button();
-            this.btnexcluir = new System.Windows.Forms.Button();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.btnpequisar = new System.Windows.Forms.Button();
-            this.dgvpedido = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.dgvPedidos = new System.Windows.Forms.DataGridView();
+            this.txtCodProduto = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvpedido)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPedidos)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Highlight;
             this.pictureBox1.Location = new System.Drawing.Point(-9, -16);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(842, 86);
             this.pictureBox1.TabIndex = 0;
@@ -59,7 +59,7 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.SystemColors.Highlight;
             this.pictureBox2.Location = new System.Drawing.Point(-4, 576);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(6);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(837, 75);
             this.pictureBox2.TabIndex = 1;
@@ -90,33 +90,34 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Loja de informático 2023";
             // 
-            // btnadicionar
+            // btnAdicionar
             // 
-            this.btnadicionar.Location = new System.Drawing.Point(15, 332);
-            this.btnadicionar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.btnadicionar.Name = "btnadicionar";
-            this.btnadicionar.Size = new System.Drawing.Size(117, 42);
-            this.btnadicionar.TabIndex = 4;
-            this.btnadicionar.Text = "adicionar";
-            this.btnadicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Location = new System.Drawing.Point(15, 332);
+            this.btnAdicionar.Margin = new System.Windows.Forms.Padding(6);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(117, 42);
+            this.btnAdicionar.TabIndex = 4;
+            this.btnAdicionar.Text = "adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnadicionar_Click);
             // 
-            // btnatuzlizar
+            // btnAtualizar
             // 
-            this.btnatuzlizar.Location = new System.Drawing.Point(141, 332);
-            this.btnatuzlizar.Name = "btnatuzlizar";
-            this.btnatuzlizar.Size = new System.Drawing.Size(117, 40);
-            this.btnatuzlizar.TabIndex = 5;
-            this.btnatuzlizar.Text = "atualizar";
-            this.btnatuzlizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Location = new System.Drawing.Point(141, 332);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(117, 40);
+            this.btnAtualizar.TabIndex = 5;
+            this.btnAtualizar.Text = "atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
             // 
-            // btnexcluir
+            // btnExcluir
             // 
-            this.btnexcluir.Location = new System.Drawing.Point(277, 332);
-            this.btnexcluir.Name = "btnexcluir";
-            this.btnexcluir.Size = new System.Drawing.Size(117, 40);
-            this.btnexcluir.TabIndex = 6;
-            this.btnexcluir.Text = "excluir";
-            this.btnexcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Location = new System.Drawing.Point(277, 332);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(117, 40);
+            this.btnExcluir.TabIndex = 6;
+            this.btnExcluir.Text = "excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -127,29 +128,29 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Cod Produto";
             // 
-            // btnpequisar
+            // btnPesquisar
             // 
-            this.btnpequisar.Location = new System.Drawing.Point(698, 338);
-            this.btnpequisar.Name = "btnpequisar";
-            this.btnpequisar.Size = new System.Drawing.Size(121, 33);
-            this.btnpequisar.TabIndex = 8;
-            this.btnpequisar.Text = "pesquisar";
-            this.btnpequisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Location = new System.Drawing.Point(698, 338);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(121, 33);
+            this.btnPesquisar.TabIndex = 8;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
             // 
-            // dgvpedido
+            // dgvPedidos
             // 
-            this.dgvpedido.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvpedido.Location = new System.Drawing.Point(15, 408);
-            this.dgvpedido.Name = "dgvpedido";
-            this.dgvpedido.Size = new System.Drawing.Size(804, 150);
-            this.dgvpedido.TabIndex = 9;
+            this.dgvPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPedidos.Location = new System.Drawing.Point(15, 408);
+            this.dgvPedidos.Name = "dgvPedidos";
+            this.dgvPedidos.Size = new System.Drawing.Size(804, 150);
+            this.dgvPedidos.TabIndex = 9;
             // 
-            // textBox1
+            // txtCodProduto
             // 
-            this.textBox1.Location = new System.Drawing.Point(583, 338);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(87, 29);
-            this.textBox1.TabIndex = 10;
+            this.txtCodProduto.Location = new System.Drawing.Point(583, 338);
+            this.txtCodProduto.Name = "txtCodProduto";
+            this.txtCodProduto.Size = new System.Drawing.Size(87, 29);
+            this.txtCodProduto.TabIndex = 10;
             // 
             // label4
             // 
@@ -168,25 +169,24 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 644);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.dgvpedido);
-            this.Controls.Add(this.btnpequisar);
+            this.Controls.Add(this.txtCodProduto);
+            this.Controls.Add(this.dgvPedidos);
+            this.Controls.Add(this.btnPesquisar);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.btnexcluir);
-            this.Controls.Add(this.btnatuzlizar);
-            this.Controls.Add(this.btnadicionar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnAdicionar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvpedido)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPedidos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -198,13 +198,13 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button btnadicionar;
-        private System.Windows.Forms.Button btnatuzlizar;
-        private System.Windows.Forms.Button btnexcluir;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnExcluir;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btnpequisar;
-        private System.Windows.Forms.DataGridView dgvpedido;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.DataGridView dgvPedidos;
+        private System.Windows.Forms.TextBox txtCodProduto;
         private System.Windows.Forms.Label label4;
     }
 }
--- RegistroDosPedidos/RegistroDosPedidos/Form1.cs
+++ RegistroDosPedidos/RegistroDosPedidos/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,12 +13,33 @@ namespace RegistroDosPedidos
 {
     public partial class Form1 : Form
     {
+        public string conexaostring;
+        private SqlConnection conexaoDB;
+        DataGridViewRow regidtroSelecionado;
         public Form1()
         {
+
             InitializeComponent();
+
+            conexaostring = "Data Source=MAR0625651W10-1;Initial Catalog=Biblioteca;Integrated Security=True";
+            conexaoDB = new SqlConnection(conexaostring);
+        
+    }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnExcluir.Enabled = false;
+            btnAtualizar.Enabled = false;
+           btnAdicionar.TabIndex = 1;
+            btnAtualizar.TabIndex = 2;
+            btnExcluir.TabIndex = 3;
+            txtCodProduto.TabIndex = 4;
+            btnPesquisar.TabIndex = 5;
+            dgvPedidos.TabIndex = 6;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+       
+
+        private void btnadicionar_Click(object sender, EventArgs e)
         {
 
         }
